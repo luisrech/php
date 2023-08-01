9761b0be23afae9816e843fc74f73a03f63a6e34
Luís Rech <116567327+luisrech@users.noreply.github.com>
2023-08-01 14:39:59 -0300

    Add files via upload

---
+++ meu primeiro site no php/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "meu primeiro script PHP";
+    // imprimir texto
+    $turmab = "vamos desenvolver um website no php";
+    // declarar variável
+    echo "<br>".$turmab;
+    // imprimira variável
+    // o br entre aspas serve pra indicar que é um elemento de html
+    $x = 5 /* +15 */ + 5;
+    echo "<br>" .$x;
+    $txt = "Hello World!";
+    $x = 5;
+    $y = 10.5;
+    echo "<br>" .$txt. "x = " .$x. ", e y = " .$y;
+    ?>
+</body>
+</html>
